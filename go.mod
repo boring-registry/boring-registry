f131ccd996dfb52283d55d260c37a2bc50e2a134
Jan Nabbefeld <jan@odise.de>
2020-12-23 22:06:57 +0100

    gcs initial

--- go.mod
+++ go.mod
@@ -3,9 +3,12 @@ module github.com/TierMobility/boring-registry
 go 1.14
 
 require (
+	cloud.google.com/go v0.66.0
+	cloud.google.com/go/storage v1.12.0 // indirect
 	github.com/aws/aws-sdk-go v1.35.34
 	github.com/fatih/color v1.7.0
 	github.com/go-kit/kit v0.10.0
+	github.com/googleapis/gax-go v1.0.3 // indirect
 	github.com/gorilla/mux v1.8.0
 	github.com/hashicorp/go-multierror v1.1.0
 	github.com/hashicorp/go-version v1.2.1
@@ -15,6 +18,8 @@ require (
 	github.com/pkg/errors v0.9.1
 	github.com/prometheus/client_golang v1.8.0
 	github.com/stretchr/testify v1.6.1
+	golang.org/x/oauth2 v0.0.0-20200902213428-5d25da1a8d43
 	golang.org/x/sys v0.0.0-20201202213521-69691e467435 // indirect
+	google.golang.org/api v0.32.0
 	gopkg.in/yaml.v3 v3.0.0-20200615113413-eeeca48fe776 // indirect
 )
---
+++ pkg/module/registry_gcs.go
@@ -0,0 +1,130 @@
+package module
+
+import (
+	"context"
+	"fmt"
+	"github.com/pkg/errors"
+	"google.golang.org/api/iterator"
+	"io"
+	"log"
+	"os"
+	"strconv"
+	//"golang.org/x/oauth2/google"
+	//"google.golang.org/api/iterator"
+
+	"cloud.google.com/go/storage"
+)
+
+// S3Registry is a Registry implementation backed by S3.
+type GCSRegistry struct {
+	sc *storage.Client
+	//s3           *s3.S3
+	//uploader     *s3manager.Uploader
+	bucket       string
+	bucketPrefix string
+	bucketRegion string
+}
+
+func (s *GCSRegistry) GetModule(ctx context.Context, namespace, name, provider, version string) (Module, error) {
+	//ctx, cancel := context.WithTimeout(ctx, time.Second*10)
+	//defer cancel()
+	o := s.sc.Bucket(s.bucket).Object(fmt.Sprintf("namespace=%[1]v/name=%[2]v/provider=%[3]v/version=%[4]v/%[1]v-%[2]v-%[3]v-%[4]v.tar.gz", namespace, name, provider, version))
+	attrs, err := o.Attrs(ctx)
+	if err != nil {
+		return Module{}, errors.Wrap(ErrNotFound, err.Error())
+	}
+	return Module{
+		Namespace:   namespace,
+		Name:        attrs.Name,
+		Provider:    provider,
+		Version:     strconv.FormatInt(attrs.Generation,10),
+		DownloadURL: attrs.MediaLink,
+	}, nil
+}
+
+func (s *GCSRegistry) ListModuleVersions(ctx context.Context, namespace, name, provider string) ([]Module, error) {
+	var modules []Module
+
+	//ctx, cancel := context.WithTimeout(ctx, time.Second*10)
+	//defer cancel()
+	query := &storage.Query{
+		Prefix: fmt.Sprintf("namespace=%s/name=%s/provider=%s", namespace, name, provider),
+		//Delimiter: "/",
+	}
+	it := s.sc.Bucket(s.bucket).Objects(ctx, query)
+	for {
+		attrs, err := it.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		module := Module{
+			Namespace:   namespace,
+			Name:        attrs.Name,
+			Provider:    provider,
+			Version:     strconv.FormatInt(attrs.Generation,10),
+			DownloadURL: attrs.MediaLink,
+		}
+
+		modules = append(modules, module)
+	}
+	return modules, nil
+}
+
+// UploadModule uploads a module to the S3 storage.
+func (s *GCSRegistry) UploadModule(ctx context.Context, namespace, name, provider, version string, body io.Reader) (Module, error) {
+	if namespace == "" {
+		return Module{}, errors.New("namespace not defined")
+	}
+
+	if name == "" {
+		return Module{}, errors.New("name not defined")
+	}
+
+	if provider == "" {
+		return Module{}, errors.New("provider not defined")
+	}
+
+	if version == "" {
+		return Module{}, errors.New("version not defined")
+	}
+
+	key := fmt.Sprintf("namespace=%[1]v/name=%[2]v/provider=%[3]v/version=%[4]v/%[1]v-%[2]v-%[3]v-%[4]v.tar.gz", namespace, name, provider, version)
+
+	if _, err := s.GetModule(ctx, namespace, name, provider, version); err == nil {
+		return Module{}, errors.Wrap(ErrAlreadyExists, key)
+	}
+
+	//ctx, cancel := context.WithTimeout(ctx, time.Second*50)
+	//defer cancel()
+	wc := s.sc.Bucket(s.bucket).Object(key).NewWriter(ctx)
+	if _, err := io.Copy(wc, body); err != nil {
+		return Module{}, errors.Wrapf(ErrUploadFailed, err.Error())
+	}
+	if err := wc.Close(); err != nil {
+		return Module{}, errors.Wrapf(ErrUploadFailed, err.Error())
+	}
+
+	return s.GetModule(ctx, namespace, name, provider, version)
+}
+
+// NewS3Registry returns a fully initialized S3 storage.
+func NewGCSRegistry(bucket string, options ...S3RegistryOption) (Registry, error) {
+	projectID := os.Getenv("GOOGLE_CLOUD_PROJECT")
+	if projectID == "" {
+		fmt.Fprintf(os.Stderr, "GOOGLE_CLOUD_PROJECT environment variable must be set.\n")
+		os.Exit(1)
+	}
+	ctx := context.Background()
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	s := &GCSRegistry{
+		sc:       client,
+		bucket:   bucket,
+	}
+	return s, nil
+}
