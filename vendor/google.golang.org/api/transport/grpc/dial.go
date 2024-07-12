f863ff61e6d04b468f7200ab524899227ddb8fb8
Olivier Michaelis <38879457+oliviermichaelis@users.noreply.github.com>
2024-07-12 21:09:34 +0200

    deps(go): update vendored dependencies
    Signed-off-by: Olivier Michaelis <38879457+oliviermichaelis@users.noreply.github.com>

--- vendor/google.golang.org/api/transport/grpc/dial.go
+++ vendor/google.golang.org/api/transport/grpc/dial.go
@@ -213,9 +213,14 @@ func dialPoolNewAuth(ctx context.Context, secure bool, poolSize int, ds *interna
 	}
 
 	// Defaults for older clients that don't set this value yet
-	var defaultEndpointTemplate string
-	if ds.DefaultEndpointTemplate == "" {
+	defaultEndpointTemplate := ds.DefaultEndpointTemplate
+	if defaultEndpointTemplate == "" {
 		defaultEndpointTemplate = ds.DefaultEndpoint
+	}
+
+	tokenURL, oauth2Client, err := internal.GetOAuth2Configuration(ctx, ds)
+	if err != nil {
+		return nil, err
 	}
 
 	pool, err := grpctransport.Dial(ctx, secure, &grpctransport.Options{
@@ -226,12 +231,14 @@ func dialPoolNewAuth(ctx context.Context, secure bool, poolSize int, ds *interna
 		GRPCDialOpts:          ds.GRPCDialOpts,
 		PoolSize:              poolSize,
 		Credentials:           creds,
+		APIKey:                ds.APIKey,
 		DetectOpts: &credentials.DetectOptions{
 			Scopes:          ds.Scopes,
 			Audience:        aud,
 			CredentialsFile: ds.CredentialsFile,
 			CredentialsJSON: ds.CredentialsJSON,
-			Client:          oauth2.NewClient(ctx, nil),
+			TokenURL:        tokenURL,
+			Client:          oauth2Client,
 		},
 		InternalOptions: &grpctransport.InternalOptions{
 			EnableNonDefaultSAForDirectPath: ds.AllowNonDefaultServiceAccount,
