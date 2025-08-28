f8c06cd1c024827406a45b851af5ef6994a0daf1
Corey Christous <cchristous@confluent.io>
2025-08-28 14:48:48 -0400

    working

--- helm_release.sh
+++ helm_release.sh
@@ -8,20 +8,29 @@ DRY_RUN=${DRY_RUN:-}
 
 repo_name=boring-registry
 chart_name=boring-registry
-chart_version=$(cat INTERNAL_VERSION)
+internal_version=$(cat INTERNAL_VERSION)
+chart_version=$(grep "^version:" "./helm/${chart_name}/Chart.yaml" | cut -d' ' -f 2)
+
+# Update Chart.yaml version if it doesn't match INTERNAL_VERSION
+if [ "${chart_version}" != "${internal_version}" ]; then
+  echo "Updating Chart.yaml version from ${chart_version} to ${internal_version}"
+  sed -i.bak "s/^version: .*/version: ${internal_version}/" "./helm/${chart_name}/Chart.yaml"
+  chart_version="${internal_version}"
+fi
+
 downloaded_chart_name="${chart_name}-${chart_version}.tgz"
 
 # Ensure cleanup on exit
 trap "rm -f ${downloaded_chart_name}" EXIT
 
 if [ -f "./helm/${chart_name}/Chart.yaml" ]; then
-  helm package "./helm/${chart_name}"
+  packaged_chart=$(helm package "./helm/${chart_name}" | grep "Successfully packaged chart" | sed 's/.*: //')
 else
   echo "chart ${chart_name} not found in ${repo_name}"
   exit 1
 fi
 
-real_chart_name=$(helm show chart "${downloaded_chart_name}" | grep "^name: " | cut -d' ' -f 2)
+real_chart_name=$(helm show chart "${packaged_chart}" | grep "^name: " | cut -d' ' -f 2)
 if [ "${real_chart_name}" != "${chart_name}" ]; then
   echo "Unexpected chart name '${real_chart_name}' (was expecting '${chart_name}')"
   exit 1
@@ -35,8 +44,8 @@ if helm show chart "${oci_path}/${chart_name}" --version "${chart_version}" 1>/d
 fi
 
 if [ -z "${DRY_RUN}" ]; then
-  echo "Pushing ${downloaded_chart_name} to ${oci_path}"
-  helm push "${downloaded_chart_name}" "${oci_path}"
+  echo "Pushing ${packaged_chart} to ${oci_path}"
+  helm push "${packaged_chart}" "${oci_path}"
 else
-  echo "Would push ${downloaded_chart_name} to ${oci_path}"
+  echo "Would push ${packaged_chart} to ${oci_path}"
 fi
