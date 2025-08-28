fe20de5ace94966a4c7ca5ed0234918fa8ea7842
Corey Christous <cchristous@confluent.io>
2025-08-28 14:36:42 -0400

    fix bugs

--- helm_release.sh
+++ helm_release.sh
@@ -1,23 +1,17 @@
 #!/usr/bin/env bash
 
-set -euo pipefail
+set -exuo pipefail
 
 ENV=${ENV:-prod}
-ECR_REPO=${ECT_REPO:-519856050701.dkr.ecr.us-west-2.amazonaws.com}
+ECR_REPO=${ECR_REPO:-519856050701.dkr.ecr.us-west-2.amazonaws.com}
 DRY_RUN=${DRY_RUN:-}
 
 repo_name=boring-registry
 chart_name=boring-registry
 chart_version=$(cat INTERNAL_VERSION)
 
-# Create a temporary directory
-temp_dir=$(mktemp -d)
-
-# Ensure cleanup on exit
-trap "rm -rf ${temp_dir}" EXIT
-
-if [ -f "${temp_dir}/helm/${chart_name}/Chart.yaml" ]; then
-  helm package "${temp_dir}/charts/${chart_name}"
+if [ -f "./helm/${chart_name}/Chart.yaml" ]; then
+  helm package "./helm/${chart_name}"
 else
   echo "chart ${chart_name} not found in ${repo_name}"
   exit 1
@@ -34,7 +28,7 @@ oci_path="oci://${ECR_REPO}/helm/${ENV}/confluentinc/${repo_name}"
 
 if helm show chart "${oci_path}/${chart_name}" --version "${chart_version}" 1>/dev/null 2>&1; then
   echo "${chart_name}:${chart_version} already exists"
-  return
+  exit 1
 fi
 
 if [ -z "${DRY_RUN}" ]; then
