33956d6cee4d91f67dc91b8224d325d5f0fe1a9e
Olivier Michaelis <38879457+oliviermichaelis@users.noreply.github.com>
2025-08-30 12:37:33 +0200

    test(auth): refactoring and additional tests (#231)

--- pkg/auth/provider_static_test.go
+++ pkg/auth/provider_static_test.go
@@ -1,8 +1,12 @@
 package auth
 
 import (
-	"github.com/stretchr/testify/assert"
+	"context"
 	"testing"
+
+	"github.com/boring-registry/boring-registry/pkg/core"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestNewStaticProvider(t *testing.T) {
@@ -30,10 +34,60 @@ func TestNewStaticProvider(t *testing.T) {
 	}
 
 	for _, tc := range testCases {
-		tc := tc
 		t.Run(tc.name, func(t *testing.T) {
 			p := NewStaticProvider(tc.tokens...).(*StaticProvider)
 			assert.ElementsMatch(t, tc.expectedTokens, p.tokens)
+		})
+	}
+}
+
+func TestStaticProvider_Verify(t *testing.T) {
+	t.Parallel()
+
+	type args struct {
+		token string
+	}
+	tests := []struct {
+		name      string
+		tokens    []string
+		args      args
+		wantError bool
+	}{
+		{
+			name:      "valid token",
+			tokens:    []string{"foo", "bar"},
+			args:      args{token: "foo"},
+			wantError: false,
+		},
+		{
+			name:      "invalid token",
+			tokens:    []string{"foo", "bar"},
+			args:      args{token: "invalid"},
+			wantError: true,
+		},
+		{
+			name:      "empty token list",
+			tokens:    []string{},
+			args:      args{token: "foo"},
+			wantError: true,
+		},
+		{
+			name:      "token with comma-separated input",
+			tokens:    []string{"foo,bar"},
+			args:      args{token: "bar"},
+			wantError: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			p := NewStaticProvider(tt.tokens...).(*StaticProvider)
+			err := p.Verify(context.Background(), tt.args.token)
+			if tt.wantError {
+				assert.ErrorIs(t, err, core.ErrInvalidToken)
+			} else {
+				assert.NoError(t, err)
+			}
 		})
 	}
 }
