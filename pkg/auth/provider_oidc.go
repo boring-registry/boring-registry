adabbb0e3521e79e9994389f49a427294bdc45ac
mickmoomam <nicholas.noonan@flocksafety.com>
2026-03-03 15:36:52 -0500

    minor: support for skipping oidc client id check in jwts

--- pkg/auth/provider_oidc.go
+++ pkg/auth/provider_oidc.go
@@ -13,15 +13,17 @@ type OidcProvider struct {
 	issuer           string
 	clientIdentifier string
 	provider         *oidc.Provider
+	skipClientIDCheck bool
 }
 
 func (p *OidcProvider) String() string { return "oidc" }
 
 // Unfortunately, it's difficult to write tests for this method, as we would need an OIDC Authorization Server
 // to generate valid signed JWTs
-func (o *OidcProvider) Verify(ctx context.Context, token string) error {
+func (o *OidcProvider) Verify(ctx context.Context, token string) error {	
 	oidcConfig := &oidc.Config{
 		ClientID: o.clientIdentifier,
+		SkipClientIDCheck: o.skipClientIDCheck,
 	}
 	verifier := o.provider.VerifierContext(ctx, oidcConfig)
 
@@ -39,7 +41,14 @@ func (o *OidcProvider) TokenURL() string {
 	return o.provider.Endpoint().TokenURL
 }
 
-func NewOidcProvider(ctx context.Context, issuer, clientIdentifier string) (*OidcProvider, error) {
+type OidcProviderOption func(*OidcProvider) *OidcProvider
+
+func WithSkipClientIDCheck(oidcProvider *OidcProvider) *OidcProvider {
+	oidcProvider.skipClientIDCheck = true
+	return oidcProvider
+}
+
+func NewOidcProvider(ctx context.Context, issuer, clientIdentifier string, options ...OidcProviderOption) (*OidcProvider, error) {
 	logger := slog.Default()
 	start := time.Now()
 	provider, err := oidc.NewProvider(ctx, issuer)
@@ -49,10 +58,16 @@ func NewOidcProvider(ctx context.Context, issuer, clientIdentifier string) (*Oid
 
 	logger.Info("finished initializing OIDC provider", slog.String("took", time.Since(start).String()))
 
-	return &OidcProvider{
+	oidcProvider := &OidcProvider{
 		logger:           logger,
 		issuer:           issuer,
 		clientIdentifier: clientIdentifier,
 		provider:         provider,
-	}, nil
+	}
+
+	for _, optionFunc := range options {
+		optionFunc(oidcProvider)
+	}
+
+	return oidcProvider, nil
 }
